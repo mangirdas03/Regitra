c0b4fceb1ffa1c298e251078fe61a8b826f04e94
redst3 <redas.steckys@ktu.edu>
2021-12-09 17:33:04 +0200

    Skaiciuokle tarsos done fully

--- RegitraISP/Controllers/TechninesApziurosController.cs
+++ RegitraISP/Controllers/TechninesApziurosController.cs
@@ -3,12 +3,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using RegitraISP.ViewModels;
+using RegitraISP.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Http;
 
 namespace RegitraISP.Controllers
 {
     public class TechninesApziurosController : Controller
     {
+        private readonly regitraispContext _context;
+        public TechninesApziurosController(regitraispContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Skaiciuokle()
         {
             return View();
         }
